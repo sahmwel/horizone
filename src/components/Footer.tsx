ce3300c854d3e43051f54bd132642c8b0e8bb444
Samuel <Samuel.stormzy1@gmail.com>
2025-10-21 10:47:09 +0100

    Prepare for deployment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { FaInstagram, FaTwitter, FaFacebook, FaTiktok } from "react-icons/fa";
+import { FaInstagram, FaTwitter, FaFacebook, FaTiktok } from "react-icons/fa6";
+
+
 
 const Footer: React.FC = () => {
   return (
